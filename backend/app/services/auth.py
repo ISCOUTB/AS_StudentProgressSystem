4d0591549a5536644a5d5b77df11169a343fa026
LaztKnightZ <lucastellanos@utb.edu.co>
2026-04-17 22:19:04 -0500

    Refactor login_user function to use database session

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,19 +1,19 @@
+from sqlalchemy.orm import Session
 from models.estudiante import Estudiantes
 from core.security import create_token
 
+def login_user(db: Session, email: str, password: str):
 
-def login_user(email: str, password: str):
+    # buscar usuario en la BD
+    user = db.query(Estudiantes).filter(Estudiantes.correo == email).first()
 
-# buscar usuario
-    for user in fake_users_db:
-    # REVISAR
-        if Estudiantes.correo == email and Estudiantes.hash_password == password:
+    if user and user.hash_password == password:
 
-            token = create_token({
-                "sub": user["email"],
-                "id": user["id"]
-            })
+        token = create_token({
+            "sub": user.correo,
+            "id": user.id
+        })
 
-            return token
+        return token
 
     return None
